0e98ba184be13c2ef65836c6c575d233dc8ea1a7
Harsha Aland <itsharshaaland@gmail.com>
2025-03-17 18:28:35 +0530

    Finding Even odd

--- first-java/src/EvenOdd.java
+++ first-java/src/EvenOdd.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class EvenOdd {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int input = in.nextInt();
+        if(input % 2 == 0) {
+            System.out.println("Given number is even");
+        } else  {
+            System.out.println("Given number is odd");
+        }
+    }
 }
