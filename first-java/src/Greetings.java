1411219366867a758fe30d474eb674e22d46aefc
Harsha Aland <itsharshaaland@gmail.com>
2025-03-17 18:31:47 +0530

    Greetings

--- first-java/src/Greetings.java
+++ first-java/src/Greetings.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Greetings {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a name: ");
+        String name = in.nextLine();
+        System.out.println("Hello " + name);
+    }
 }
