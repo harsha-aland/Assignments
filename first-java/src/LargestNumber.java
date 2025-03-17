02cbce879a291bd5711852aa9905e638e06e8cdb
Harsha Aland <itsharshaaland@gmail.com>
2025-03-17 18:57:55 +0530

    Find largest number

---
+++ first-java/src/LargestNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        Scanner in1 = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
+
+        int num1 = in1.nextInt();
+        int num2 = in2.nextInt();
+        if (num1 == num2) {
+            System.out.println("Both are equal");
+        } else if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else {
+            System.out.println(num2 + " is greater than " + num1);
+        }
+    }
+}
