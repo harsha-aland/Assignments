c32c9b3ae65f4913191a81dcd0ef89737349edcb
Harsha Aland <itsharshaaland@gmail.com>
2025-03-21 10:08:33 +0530

    Condition to check if the target is greater than the greatest number

--- Binar Search/src/Ceiling.java
+++ Binar Search/src/Ceiling.java
@@ -1,7 +1,7 @@
 public class Ceiling {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 5, 7, 18, 19, 21, 32};
-        int target = 20;
+        int target = 33;
         System.out.println((ceilingOfaNumber(arr, target)));
     }
 
@@ -9,6 +9,9 @@ public class Ceiling {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
+            if(target > arr[arr.length-1]){
+                return -1;
+            }
             int mid = start + (end - start) / 2;
             if (target < arr[mid]) {
                 end = mid - 1;
