d360c7adcbe692d310b67ceab234f8db32b3ac61
Harsha Aland <itsharshaaland@gmail.com>
2025-03-24 23:38:47 +0530

    Missing number using cyclic sort

---
+++ Binar Search/src/MissingNumber.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        missingNumber(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    //https://leetcode.com/problems/missing-number/submissions/1584733903/
+    static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correctIndex]) {
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (j != nums[j]) {
+                return j;
+            }
+        }
+        return nums.length;
+    }
+}
